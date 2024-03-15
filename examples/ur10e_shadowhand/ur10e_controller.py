4d5ba865c1602bccf5d9c8a089f731ed4b1333a3
SHEN YIBO <56157591+Spphire@users.noreply.github.com>
2024-03-15 18:25:36 +0800

    Update ur10e_controller.py

--- examples/ur10e_shadowhand/ur10e_controller.py
+++ examples/ur10e_shadowhand/ur10e_controller.py
@@ -6,6 +6,7 @@ import threading
 import json
 import requests
 
+from curobo.geom.types import WorldConfig, Cuboid
 from curobo.types.robot import JointState, RobotConfig
 from curobo.util_file import get_robot_configs_path, join_path, load_yaml
 from curobo.types.base import TensorDeviceType
@@ -13,17 +14,32 @@ from curobo.wrap.reacher.mpc import MpcSolver, MpcSolverConfig
 from curobo.wrap.reacher.motion_gen import MotionGen, MotionGenConfig, MotionGenPlanConfig
 from curobo.geom.types import WorldConfig
 from curobo.types.math import Pose
+from curobo.rollout.rollout_base import Goal
+
+
+def get_custom_world_model(table_height=0.02):
+    table = Cuboid(
+        name="table",
+        dims=[4.0,4.0,4.0],
+        pose=[0.0, 0.0, -2.0+table_height, 1.0, 0, 0, 0],
+        color=[0, 1.0, 0, 1.0],
+    )
+    return WorldConfig(
+        cuboid=[table],
+    )
 
 class Ur10eController():
     def __init__(self,
                  world_model:WorldConfig,
                  ros_ip="10.53.21.95",
-                 ros_port=8000,):
+                 ros_port="8000"):
         self.homing_state=False
         self.tracking_state=False
 
         self.tensor_args = TensorDeviceType()
         self.world_model = world_model
+        self.ros_ip = ros_ip
+        self.ros_port = ros_port
         self.joint_names = ['ra_shoulder_pan_joint', 'ra_shoulder_lift_joint', 
                             'ra_elbow_joint', 'ra_wrist_1_joint', 
                             'ra_wrist_2_joint', 'ra_wrist_3_joint']
@@ -31,12 +47,18 @@ class Ur10eController():
         self.DOF=6
 
         try:
-            q = requests.post("http://10.53.21.95:8000/getJoints")
+            q = requests.post("http://"+self.ros_ip+":"+self.ros_port+"/getJoints")
             if len(list(json.loads(q.content).values()))==self.DOF:
                 print("connected to ros!")
+                self.init_mpc()
+            else:
+                print("DOF wrong")
+                
         except:
             print("Failed to connect to ros!")
             quit()
+
+        
 
     def init_mpc(self,config_name="ur10e.yml"):
         robot_cfg = load_yaml(join_path(get_robot_configs_path(), config_name))["robot_cfg"]
@@ -54,9 +76,22 @@ class Ur10eController():
             step_dt=0.02,
         )
         self.mpc = MpcSolver(self.mpc_config)
+        self.retract_cfg = self.mpc.rollout_fn.dynamics_model.retract_config.unsqueeze(0)
+        retract_state = JointState.from_position(self.retract_cfg, joint_names=self.mpc.rollout_fn.joint_names)
+        state = self.mpc.rollout_fn.compute_kinematics(retract_state)
+        retract_pose = Pose(state.ee_pos_seq, quaternion=state.ee_quat_seq)
         
-
         self.init_retract(self.mpc.rollout_fn.dynamics_model.retract_config.unsqueeze(0))
+
+        goal = Goal(
+            current_state=self.get_current_jointstate(),
+            goal_state=self.get_current_jointstate(),
+            goal_pose=self.get_current_Pose(),
+        )
+        self.goal_buffer = self.mpc.setup_solve_single(goal, 1)
+        self.mpc.update_goal(self.goal_buffer)
+
+        
 
         self.start_real_tcp = self.get_current_tcp()
         self.start_unity_tcp = np.zeros(7)
@@ -64,6 +99,8 @@ class Ur10eController():
 
         self.past_pose = None
         self.past_rot = None
+        print("mpc inited!")
+
 
     def init_retract(self,tensor):
         self.retract_cfg = tensor 
@@ -85,7 +122,7 @@ class Ur10eController():
         
     def get_current_q(self) -> List[float]:
         try:
-            q = requests.post("http://10.53.21.95:8000/getJoints")
+            q = requests.post("http://"+self.ros_ip+":"+self.ros_port+"/getJoints")
             if len(list(json.loads(q.content).values()))==self.DOF:
                 return list(json.loads(q.content).values())
         except:
@@ -119,11 +156,11 @@ class Ur10eController():
         )
     
     def can_move(self):
-        return (not self.locked) and (self.homing_state or self.tracking_state)
+        return not self.homing_state and self.tracking_state
 
     def move(self, target_q: List[float]):
         if self.can_move():
-            requests.post("http://10.53.21.95:8000/move",json.dumps({'q': target_q}))
+            requests.post("http://"+self.ros_ip+":"+self.ros_port+"/move",json.dumps({'q': target_q}))
 
     def set_start_tcp(self, pos_quat:np.ndarray):
         self.start_real_tcp = self.get_current_tcp()
@@ -155,16 +192,19 @@ class Ur10eController():
             np.linalg.norm(target_position - self.past_pose) > 1e-2 
             or np.linalg.norm(target_orientation - self.past_rot) > 1e-3
         ):
-
+            
             ik_goal = Pose(
                 position=self.tensor_args.to_device(target_position),
                 quaternion=self.tensor_args.to_device(target_orientation),
             )
-
+            
             self.goal_buffer.goal_pose.copy_(ik_goal)
+            #print("-mpc...")
             self.mpc.update_goal(self.goal_buffer)
+            #print("mpc..")
             self.past_pose = target_position
             self.past_rot = target_orientation
+            
 
         mpc_result = self.mpc.step(self.get_current_jointstate(), max_attempts=2)
         state = mpc_result.js_action.position.cpu().numpy().reshape(self.DOF)
